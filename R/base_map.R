a226f6947747a80ef00573c37742404005525ebe
Robert Crump <119753396+rwrcrmp@users.noreply.github.com>
2023-05-23 18:30:05 -0500

    config 5.23

--- R/base_map.R
+++ R/base_map.R
@@ -1,18 +1,14 @@
-# base map script
-
 library(tidyverse)
 library(sf)
 
 # Prelim setup ------------------------------------------------------------
 
-# https://tnr-traviscountytx.opendata.arcgis.com/datasets/TravisCountyTX::boundary/explore?location=30.324646%2C-97.770000%2C10.82
-travis_boundary <- st_read("Desktop/atx_crash_data/Boundary/Boundary.shp")
+travis_boundary <- st_read("data/atx_crash_data/Boundary/Boundary.shp")
 
 ggplot(travis_boundary) +
   geom_sf()
-
-# https://data.austintexas.gov/dataset/Street-Centerline/8hf2-pdmb
-road_segments <- st_read("Desktop/atx_crash_data/Street Centerline/geo_export_99c4ed0d-9896-47a9-9377-143227d0fbc8.shp")
+ 
+road_segments <- st_read("data/atx_crash_data/Street Centerline/geo_export_99c4ed0d-9896-47a9-9377-143227d0fbc8.shp")
 
 road_segments <- st_transform(road_segments, st_crs(travis_boundary))
 
---
+++ data/README.data.md
@@ -0,0 +1,17 @@
+## data info and links:
+
+Travis County bounday shapefile:
+
+https://tnr-traviscountytx.opendata.arcgis.com/datasets/TravisCountyTX::boundary/explore?location=30.324646%2C-97.770000%2C10.82
+
+Austin streets centerline shapefile:
+
+https://data.austintexas.gov/dataset/Street-Centerline/8hf2-pdmb
+
+Austin crash data API link:
+
+https://data.austintexas.gov/resource/y2wy-tgr5.json
+
+TXDOT roadway inventory:
+
+https://www.txdot.gov/data-maps/roadway-inventory.html
